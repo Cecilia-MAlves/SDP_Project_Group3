003a55803fed8f62932f5ddb71297a8e90e46f0f
Cecilia Machado <ceciliamachado@Cecilias-MacBook-Air.local>
2024-11-20 21:45:38 +0100

    cleaning tutorial unused files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,7 @@
 import os
 import tempfile
-
 import pytest
 from app import create_app
-from app.db import get_db, init_db
-
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
-
-
-@pytest.fixture
-def app():
-    db_fd, db_path = tempfile.mkstemp()
-
-    app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
-
-    with app.app_context():
-        init_db()
-        get_db().executescript(_data_sql)
-
-    yield app
-
-    os.close(db_fd)
-    os.unlink(db_path)
 
 
 @pytest.fixture
@@ -33,7 +9,6 @@ def client(app):
     app = create_app()
     app.testing = True
     return app.test_client()
-
 
 @pytest.fixture
 def runner(app):
