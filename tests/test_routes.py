94557765cca3780d30da1ef76a0dde890b8f6025
Cecilia Machado <ceciliamachado@Cecilias-MacBook-Air.local>
2024-12-16 15:29:13 +0100

    Adding 2 unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,6 +8,27 @@ def test_cpu_temp(mocker, client):
     assert response.json == {"Temperature (C)": 45.0}
 
 
+def test_cpu_temp_status_fine(mocker, client):
+    # Mock the CPUTemperature class
+    mock_cpu = mocker.patch('app.routes.read_cpu_temperature')
+    mock_cpu.return_value = 45.0
+
+    response = client.get('/cpu/temp/error') 
+    assert response.status_code == 200
+    assert response.json == {"Temperature (C)": 45.0}
+    assert response.json == {"Status": "fine"}
+
+
+def test_cpu_temp_status_hot(mocker, client):
+    # Mock the CPUTemperature class
+    mock_cpu = mocker.patch('app.routes.read_cpu_temperature')
+    mock_cpu.return_value = 65.0
+
+    response = client.get('/cpu/temp/error') 
+    assert response.status_code == 200
+    assert response.json == {"Temperature (C)": 65.0}
+    assert response.json == {"Status": "too hot"}
+
 def test_disk_usage(mocker, client):
     # Mock the shutil.disk_usage function
     mock_disk = mocker.patch('app.routes.shutil.disk_usage')
